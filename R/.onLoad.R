877c694a2f0e4235a3cd39f59fb1f09483f96edb
esimms999 <esimms@yahoo.com>
2023-09-28 14:04:27 +0000

    Vignette on package date for pkgdown.

--- R/.onLoad.R
+++
@@ -1,8 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-  options(yaml.eval.expr = TRUE)
-
-  packageStartupMessage("Setting options(yaml.eval.expr=TRUE) via .onLoad")
-  cat("Hello from me!")
-
-  run_on_load()
-}
